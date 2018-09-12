eaafcc34fe8ed884b1d955b89977c37bfb8b1f6f
The-End-Hero <527409987@qq.com>
2018-09-12 16:29:28 +0800

    update

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,11 @@
-declare const Pinyin: any;
+declare const Pinyin: {
+    compare: typeof compare;
+    compareAll: typeof compareAll;
+    sort: (obj: any, key: any) => any;
+    search: (str: any, search: any) => boolean;
+    getFilterArr: (arr: any, search: any, key?: string) => any;
+    getFilterArrAll: (arr: any, search: any) => any;
+};
+declare function compare(val1: any, val2: any): number | undefined;
+declare function compareAll(val1: any, val2: any): any;
 export default Pinyin;
